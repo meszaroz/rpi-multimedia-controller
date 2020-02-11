cbd6e5a853c5bfe36b04dcea7ef34737f4e3d6ac
Xperion <zoltan.meszaros@xperion.hu>
2020-02-11 10:09:09 +0100

    Fixed bug: sync containerSize and writeDataToBuffer

--- rpi-multimedia-controller/Common/mstatus.c
+++ rpi-multimedia-controller/Common/mstatus.c
@@ -40,8 +40,8 @@ static TSize containerSize(const StatusContainer* const cont) {
 
 static bool writeDataToBuffer(Buffer *buffer, StatusContainer *cont) {
     bool out = buffer && cont && isBufferAtBeginning(buffer);
-    if (out) {
-        out = writeDataString(buffer, cont->act ? cont->act : cont->act) && out;
+    if (out && cont->act) {
+        out = writeDataString(buffer, cont->act) && out;
         out = writeDataWithSize(buffer,(TPointer*)(&cont->play),sizeof(TBool    )) && out;
         out = writeDataWithSize(buffer,(TPointer*)(&cont->dura),sizeof(TDuration)) && out;
         out = writeDataWithSize(buffer,(TPointer*)(&cont->pos ),sizeof(TSeek    )) && out;
